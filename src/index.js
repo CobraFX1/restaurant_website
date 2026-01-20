6af0303badf13a1ea501595b7fe0b15ac40d6bb6
Jacob Stephen <Cashflow34steph@gmail.com>
2026-01-20 04:08:24 +0100

    Initial Design for the homepage

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@
 // import drink from "./images/cocktail.jpg";
 // import appetizer from "./images/golden_beet3.jpg";
 // import favicon from "./images/cutlery.png";
-
-
+import { render } from "initial-page-load.js";
 console.log("yo, I'm testing");
